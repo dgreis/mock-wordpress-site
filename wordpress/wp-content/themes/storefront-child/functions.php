047f48cdd4fd25c3ff26cc5ea9d362ce4273295e
David Greis <2243125+dgreis@users.noreply.github.com>
2019-04-25 13:23:05 +0300

    add woocommerce support in functions.php and also include cart.php which is a template I will override

--- wordpress/wp-content/themes/storefront-child/functions.php
+++ wordpress/wp-content/themes/storefront-child/functions.php
@@ -1,9 +1,17 @@
 <?php
+
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
  
 }
+
+
+function mytheme_add_woocommerce_support() {
+	add_theme_support( 'woocommerce' );
+}
+add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+
 
 function generateRandomString($length = 10) {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
