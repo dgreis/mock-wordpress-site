4b4e6006ce19dcdfdbbbc20d8ba9ad6906d4efb7
David Greis <2243125+dgreis@users.noreply.github.com>
2019-04-25 10:28:03 +0300

    added function to create new coupon in widget; moved code into functions.php

--- wordpress/wp-content/themes/storefront-child/functions.php
+++ wordpress/wp-content/themes/storefront-child/functions.php
@@ -4,4 +4,46 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
  
 }
+
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+//$coupon_code = generateRandomString();
+
+function createDBCoupon($coupon_code){
+	/**
+	 * Create a coupon programatically
+	 */
+	//$coupon_code = $coupon_code; // Code
+	$amount = '10'; // Amount
+	$discount_type = 'fixed_cart'; // Type: fixed_cart, percent, fixed_product, percent_product
+						
+	$coupon = array(
+		'post_title' => $coupon_code,
+		'post_content' => '',
+		'post_status' => 'publish',
+		'post_author' => 1,
+		'post_type'		=> 'shop_coupon'
+	);
+						
+	$new_coupon_id = wp_insert_post( $coupon );
+						
+	// Add meta
+	update_post_meta( $new_coupon_id, 'discount_type', $discount_type );
+	update_post_meta( $new_coupon_id, 'coupon_amount', $amount );
+	update_post_meta( $new_coupon_id, 'individual_use', 'no' );
+	update_post_meta( $new_coupon_id, 'product_ids', '' );
+	update_post_meta( $new_coupon_id, 'exclude_product_ids', '' );
+	update_post_meta( $new_coupon_id, 'usage_limit', '' );
+	update_post_meta( $new_coupon_id, 'expiry_date', '' );
+	update_post_meta( $new_coupon_id, 'apply_before_tax', 'yes' );
+	update_post_meta( $new_coupon_id, 'free_shipping', 'no' );
+}
 ?>
